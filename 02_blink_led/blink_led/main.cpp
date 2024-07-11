adab0156c3dcd0d26cdd32ad8d0753c24665c28f
sigmdel <michel@sigmdel.ca>
2024-07-11 20:36:11 -0300

    Avoid stating that an output of HIGH always turns LED on

--- 02_blink_led/blink_led/main.cpp
+++ 02_blink_led/blink_led/main.cpp
@@ -5,30 +5,34 @@
 //   Getting Started with Seeed Studio XIAO ESP32C3
 //   @ https://wiki.seeedstudio.com/XIAO_ESP32C3_Getting_Started/
 
-
-#include <Arduino.h>  // Needed in PlatformIO
+#include <Arduino.h>
 
 // Connecting an external LED:
 //  The diode's cathode (-, usually the short lead on the flat side of the LED) is connected to GND.
 //  The diode's anode (+, usually the long lead on the round side of the LED) is connected to a
 //  current limiting 240 ohm resistor. The other lead of the resistor is connected to an I/O pin.
+//  See https://files.seeedstudio.com/wiki/XIAO_WiFi/connect-led-2.png
 //
-int led = D10;
+const int ledPin = D10;
+const int ledOn = HIGH;
 
 void setup() {
-  // Set the digital pin connected to the LED as an output
-  pinMode(led, OUTPUT);
-
   Serial.begin();
-  delay(1000);      // 1 second delay should be sufficient for USB-CDC
+  delay(2000); // 2 second delay should be sufficient for USB-CDC
+
+  Serial.printf("Set the mode of I/O pin %d connected to the LED to OUTPUT\n", ledPin);
+  pinMode(ledPin, OUTPUT);
+
   Serial.println("Setup completed");
 }
 
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on
+  digitalWrite(ledPin, ledOn);
   Serial.print("ON, ");
-  delay(50);                 // for 1/20th of a second
-  digitalWrite(led, LOW);    // turn the LED off
+  // LED on for 1/20th of a second
+  delay(50);
+  digitalWrite(ledPin, 1-ledOn);
   Serial.println("Off...");
-  delay(1000);               // for a second
+  // LED off for a second
+  delay(1000);
 }
